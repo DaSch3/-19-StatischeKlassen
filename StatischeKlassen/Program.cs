3df79145da597ffb25b23fedd172adf802f6b122
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:39:54 +0200

    Projektdateien hinzufügen.

---
+++ StatischeKlassen/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatischeKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ergebnis = Mathematik.Multiplikation(10, 2);
+
+            Console.WriteLine(ergebnis);
+            Console.ReadKey();
+        }
+    }
+
+    static class Mathematik
+    {
+        public static int Addition(int wert1, int wert2)
+        {
+            return wert1 + wert2;
+        }
+        public static int Subtraktion(int wert1, int wert2)
+        {
+            return wert1 - wert2;
+        }
+        public static int Division(int wert1, int wert2)
+        {
+            return wert1 / wert2;
+        }
+        public static int Multiplikation(int wert1, int wert2)
+        {
+            return wert1 * wert2;
+        }
+        public static int Modulo(int wert1, int wert2)
+        {
+            return wert1 % wert2;
+        }
+    }
+}
